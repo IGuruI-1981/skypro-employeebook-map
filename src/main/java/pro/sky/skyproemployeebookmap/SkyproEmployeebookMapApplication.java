06820fe5aea1160d9b95d7b30a545561f0331e95
Viktor <viktor.ihtiyarov@gmail.com>
2022-10-09 18:07:09 +0700

    Initial commit

---
+++ src/main/java/pro/sky/skyproemployeebookmap/SkyproEmployeebookMapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproemployeebookmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproEmployeebookMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproEmployeebookMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyproemployeebookmap/SkyproEmployeebookMapApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproemployeebookmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproEmployeebookMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
